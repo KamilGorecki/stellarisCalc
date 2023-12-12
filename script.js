85432cd1cddce19ff5797857fb5535b0d9a9fbc4
KamilGorecki <gorecki.k@gmail.com>
2023-12-12 22:16:09 +0100

    remove leviathan and add speedups calc

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 const links = [
   { name: "Home", src: "/" },
-  { name: "Leviathan", src: "/src/leviathan.html" },
+  { name: "SUCalc", src: "/src/speedups.html" },
 ];
+
+const speedUps = [1, 5, 15, 60, 180, 480, 1440, 4320];
 
 const header = document.querySelector("header");
 const nav = document.createElement("nav");
 const su = document.querySelectorAll("input");
 const total = document.getElementById("su-total");
-const shipTime = document.getElementById("ship-time");
-const shipsTotal = document.getElementById("ships-total");
 
-let sum = 0;
-let shipSum = 0;
-let ships = 0;
-
-let boost = 0;
 header.append(nav);
 
 links.forEach((elem) => {
@@ -34,43 +29,14 @@ function count() {
   getAmount();
 }
 
+// Priapus sucks balls hard, thats why he is so salty.
+
+// And for his son .... no more IF statment. Sorry Mr Senior Dev.
+
 function getAmount() {
-  const su = document.querySelectorAll("input");
-
-  su.forEach((elem) => {
-    let i = parseInt(elem.value);
-    if (elem.name === "1m") {
-      sum = parseInt(sum) + i;
-    } else if (elem.name === "5m") {
-      sum = parseInt(sum) + i * 5;
-    } else if (elem.name === "15m") {
-      sum = parseInt(sum) + i * 15;
-    } else if (elem.name === "1h") {
-      sum = parseInt(sum) + i * 60;
-    } else if (elem.name === "3h") {
-      sum = parseInt(sum) + i * 180;
-    } else if (elem.name === "8h") {
-      sum = parseInt(sum) + i * 480;
-    } else if (elem.name === "1d") {
-      sum = parseInt(sum) + i * 1440;
-    } else if (elem.name === "3d") {
-      sum = parseInt(sum) + i * 4320;
-    } else if (elem.name === "shipTime") {
-      shipSum = parseInt(i) * 60;
-    } else if (elem.name === "boost") {
-      if (i != 0) {
-        boost = 1 - parseInt(i) / 100;
-      }
-    }
+  su.forEach((element, i) => {
+    sum = parseInt(sum) + parseInt(element.value) * speedUps[i];
   });
-
-  if (boost > 0 && boost < 1) {
-    console.log(boost);
-    shipSum = shipSum * boost;
-    ships = sum / shipSum;
-  }
-  ships = sum / shipSum;
-  shipsTotal.innerHTML = Math.floor(ships);
   total.innerHTML = convertToDays(sum);
 }
 
